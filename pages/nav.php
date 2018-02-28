0c79259f4c4ff7e753ca52216998e458ebd6f8bc
msadeeke <msadeeke@msadeekes-MacBook-Pro.local>
2018-02-28 11:24:03 -0500

    new update

---
+++ pages/nav.php
@@ -0,0 +1,34 @@
+<nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li class="active"><a href="index.html">Home</a></li>
+          <!-- <li><a class="drop" href="#">Pages</a>
+            <ul>
+              <li><a href="pages/gallery.html">Gallery</a></li>
+              <li><a href="pages/full-width.html">Full Width</a></li>
+              <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>
+              <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
+              <li><a href="pages/basic-grid.html">Basic Grid</a></li>
+            </ul>
+          </li> -->
+          <li><a class="drop" href="pages/experiences.html">Experiences</a>
+            <ul>
+              <li><a class="drop" href="#">Leadership Team</a>
+                <ul>
+                  <li><a href="pages/mayor.html">Karen Majewski</a></li>
+                  <li><a href="pages/rashida.html#">Rashida Tlaib</a></li>
+                  <li><a href="pages/ali.html">Suleiman Ali Suleiman</a></li>
+                  <li><a href="pages/mitch.html">Mitchell Shamsuddin</a></li>
+                </ul>
+                <li><a class="drop" href="#">Executive</a>
+                  <ul><li><a href="pages/seema.html">Seema Ahmed</a></li></ul>
+              </li>
+            </ul>
+          </li>
+          <li><a href="pages/volunteers.html">Volunteers</a>
+          <li><a href="pages/support.html" target="_blank"></a>
+          <li><a href="pages/contact_us.html">Contact Us</a>
+          
+  
+          </li>
+        </ul>
+      </nav>
